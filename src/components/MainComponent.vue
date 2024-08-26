c730bd9ae296196901b4d7f1c310832fb31e044b
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-08-26 19:19:00 +0200

    created store.js initial data

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -6,6 +6,7 @@
                 <h5 class="card-title">Destinazione Viaggio</h5>
                 <h6 class="card-subtitle mb-2 text-body-secondary">Data inizio e fine viaggio</h6>
                 <p class="card-text">Descrizione sintetica viaggio</p>
+                <button type="button" class="btn btn-success btn-sm">Aggiungi nuova tappa</button>
             </div>
         </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,29 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-
+    travels: [{
+        destination: '',
+        tripdate:'',
+        rating: '',
+        description: '',
+        map:'',
+      },
+    ],
+    
+    viaggi: [{
+      destinazione: "Roma, Italia",
+      data: "2024-09-15",
+      valutazione: 4.8,
+      dettaglio: {
+        immagini: [
+          "https://example.com/roma1.jpg",
+          "https://example.com/roma2.jpg"
+        ],
+        coordinate_geografiche: {
+          "latitudine": 41.9028,
+          "longitudine": 12.4964
+        },
+        note: "Visita al Colosseo e al Vaticano."
+        }
+    }],
 })
