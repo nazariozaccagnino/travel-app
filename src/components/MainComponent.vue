fdcc2e3393599824c6e3546ad9cd27ef76f6ef8e
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-08-27 18:07:31 +0200

    fixed filling of json

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -7,14 +7,14 @@
                     <h5 class="card-title">{{ item.destination }}</h5>
                     <h6 class="card-subtitle mb-2 text-body-secondary">Data inizio e fine viaggio</h6>
                     <p class="card-text">Descrizione sintetica viaggio</p>
-                    <button type="button" class="btn btn-success btn-sm">Aggiungi nuova tappa</button>
+                    <button type="button" class="btn btn-success btn-sm" @click="addLeg(this.newLeg, index)">Aggiungi nuova tappa</button>
                 </div>
             </div>
-            <div v-for="childItems in item.details">
+            <div v-for="childItems in item.details" :key="index">
                 <div class="card" style="width: 18rem;">
                     <img src="..." class="card-img-top" alt="MAPPA">
                     <div class="card-body">
-                        <h5 class="card-title">{{ childItems }}</h5>
+                        <h5 class="card-title">{{ childItems.voce1 }}</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
                             the
                             card's content.</p>
@@ -35,24 +35,18 @@ export default {
             store,
             errorInputName: false,
             newTrip: {
-                destination: 'a',
+                destination: '',
                 tripdate: '',
                 rating: '',
-                details: [{
-                    voce1: 'a',
-                    voce2:'b'
-                },
-                {
-                    voce1: 'a',
-                    voce2:'b'
-                }]
+                details: []
             },
-            newleg: {
-                images: [],
+            newLeg: {
+                name: 'leg',
                 place: {
                     "latitude": 0,
                     "longitude": 0
                 },
+                images: [],
                 notes: '',
             }
         }
@@ -67,12 +61,21 @@ export default {
                 console.log(this.newTrip);
                 this.store.travels.push({ ...newTrip })
                 console.log(this.store.travels);
-                console.log(this.store.travels.details);
-
-
+                this.newTrip.destination= '';
+                this.newTrip.tripdate= '';
+                this.newTrip.rating= '';
+                this.newTrip.details= [];
             }
         },
-        mounted() {
+        addLeg(newLeg, index){            
+                this.errorInputName = false;
+                console.log(index);             
+                console.log(this.newLeg);
+                this.store.travels[index].details.push({ ...newLeg })
+                console.log(this.store.travels);
+        },
+        computed() {
+            
         }
     }
 }
