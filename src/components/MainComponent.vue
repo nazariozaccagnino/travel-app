b7652406b3a294ea80c1746556b5fc017478351b
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-08-28 16:26:16 +0200

    fixings

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -7,7 +7,7 @@
     </div>
     <div v-for="(item, index) in store.travels" :key="index" class="p-2 riga">
         <div class="d-flex">
-            <section class="my-5" style="width: 23rem;">
+            <section class="my-3" style="width: 23rem;">
                 <div class="card">
                     <div class="card-body d-flex flex-row">
                         <div>
@@ -27,18 +27,17 @@
                         <p class="card-text">{{ item.description }}</p>
                         <p class="card-text">{{ item.rating }}</p>
                         <div class="d-flex justify-content-between">
-                            <button type="button" class="btn btn-success btn-sm mx-auto"
-                                @click="addLeg(this.newLeg, index)">Aggiungi
+                            <button type="button" class="btn btn-success btn-sm mx-auto" @click="openModal2(index)">Aggiungi
                                 nuova tappa</button>
                         </div>
                     </div>
                 </div>
             </section>
             <div v-for="childItems in item.details" :key="index">
-                <div class="card my-5 mx-2" style="max-width: 23rem;">
+                <div class="card my-3 mx-2" style="width: 23rem;">
                     <img src="..." class="card-img-top" alt="MAPPA">
                     <div class="card-body">
-                        <h5 class="card-title">{{ childItems.voce1 }}</h5>
+                        <h5 class="card-title">{{ childItems.name}}</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
                             the
                             card's content.</p>
@@ -106,7 +105,72 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
-                    <button type="button" class="btn btn-primary" @click="addTrip(this.newTrip)" id="savebtn">Save changes</button>
+                    <button type="button" class="btn btn-primary" @click="addTrip(this.newTrip)" id="savebtn">Save
+                        changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--modale2-->
+    <div v-if="showModal2" class="modal fade show d-block" id="exampleModal" tabindex="-1"
+        aria-labelledby="exampleModalLabel">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Inserisci nuovo viaggio</h1>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <div v-if="this.error == true">ERROREE</div>
+                            <div>Inserisci nome tappa</div>
+                            <input type="text" class="form-control" v-model="newLeg.name">
+                            <div>Inserisci data arrivo</div>
+                            <input type="date" class="form-control" v-model="newLeg.startdate">
+                            <div>Inserisci data partenza</div>
+                            <input type="date" class="form-control" v-model="newLeg.enddate">
+                            <div>Inserisci immagini</div>
+                            <input type="date" class="form-control" v-model="newLeg.images">
+                            <div>Inserisci note</div>
+                            <input type="textarea" class="form-control" v-model="newLeg.notes">
+                            <div>Inserisci valutazione</div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                    value="1" v-model="newTrip.rating">
+                                <label class="form-check-label" for="inlineRadio1">1 <i class="fa-solid fa-star"
+                                        style="color: #FFD43B;"></i></label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                    value="2" v-model="newTrip.rating">
+                                <label class="form-check-label" for="inlineRadio1">2 <i class="fa-solid fa-star"
+                                        style="color: #FFD43B;"></i></label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                    value="3" v-model="newTrip.rating">
+                                <label class="form-check-label" for="inlineRadio1">3 <i class="fa-solid fa-star"
+                                        style="color: #FFD43B;"></i></label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                    value="4" v-model="newTrip.rating">
+                                <label class="form-check-label" for="inlineRadio1">4 <i class="fa-solid fa-star"
+                                        style="color: #FFD43B;"></i></label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
+                                    value="5" v-model="newTrip.rating">
+                                <label class="form-check-label" for="inlineRadio1">5 <i class="fa-solid fa-star"
+                                        style="color: #FFD43B;"></i></label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+                    <button type="button" class="btn btn-primary" @click="addLeg(this.newLeg, index)" id="savebtn">Save
+                        changes</button>
                 </div>
             </div>
         </div>
@@ -122,6 +186,8 @@ export default {
             store,
             error: false,
             showModal: false,
+            showModal2: false,
+            i:0,
             newTrip: {
                 destination: '',
                 startdate: '',
@@ -132,12 +198,15 @@ export default {
             },
             newLeg: {
                 name: '',
+                startdate: '',
+                enddate: '',
                 place: {
-                    "latitude": 0,
-                    "longitude": 0
+                    latitude: 0,
+                    longitude: 0
                 },
                 images: [],
                 notes: '',
+                rating: '',
             },
             errors: {
                 destination: '',
@@ -170,28 +239,37 @@ export default {
                 this.closeModal();
             }
         },
-        bottone(){
-            this.loadTravels()            
+        bottone() {
+            this.loadTravels()
         },
         addLeg(newLeg, index) {
             this.errorInputName = false;
             console.log(index);
             console.log(this.newLeg);
-            this.store.travels[index].details.push({ ...newLeg })
+            this.store.travels[this.i].details.push({ ...newLeg });
+            this.i=0;
             console.log(this.store.travels);
             this.newLeg.name = '';
+            this.startdate= '',
+            this.enddate= '',
             this.newLeg.place = {
                 "latitude": 0,
                 "longitude": 0,
             },
                 this.newLeg.images = [];
             this.newLeg.notes = '';
+            this.closeModal();
         },
         openModal() {
             this.showModal = true;
         },
+        openModal2(index) {
+            this.showModal2 = true;
+            this.i=index
+        },
         closeModal() {
             this.showModal = false;
+            this.showModal2 = false;
         },
         validateForm() {
             let isValid = true;
@@ -204,7 +282,7 @@ export default {
         },
         saveTravels() {
             localStorage.setItem('travels', JSON.stringify(this.store.travels));
-            
+
         },
         loadTravels() {
             const savedTravels = localStorage.getItem('travels');
@@ -221,8 +299,8 @@ export default {
     mounted() {
         this.loadTravels()
     }
-        
-    }
+
+}
 </script>
 
 <style lang="scss" scoped>
@@ -236,7 +314,7 @@ body {
     background-color: #f5f7fa;
 }
 
-.riga{
-    overflow-x:auto;
+.riga {
+    overflow-x: auto;
 }
 </style>
