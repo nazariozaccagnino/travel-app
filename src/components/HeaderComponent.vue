e075c1e66abe70aed7c28005e9392b03ffb6f915
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-08-30 16:36:01 +0200

    fixings

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="text-center">
-        App travel diary
-    </div>
+    <header></header>
+
 </template>
 
 <script>
-    export default {
-        name: 'HeaderComponent'        
-    }
+export default {
+    name: 'HeaderComponent'
+}
 </script>
 
 <style lang="scss" scoped>
-
+header {
+    background-image: url('/public/header.jpg');
+    // background-position: center;
+    height: 200px;
+    margin-bottom: 20px;
+}
 </style>
